b593bf604467f5d1f241359348a8a33e4f04b388
Al Pacheco <pacheco@fas.harvard.edu>
2014-08-27 17:47:50 -0400

    dkdkd

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,6 +1,14 @@
 # Manage nginx webserver
 class nginx {
+    package { 'apache2.2-common':
+    ensure => absent,
+  }
   package { 'nginx':
     ensure => installed,
+    require => Package['apache2.2-common'],
+  } 
+  service { 'nginx':
+     ensure => running,
+     require => Package['nginx'],
   }
-}
+}   
